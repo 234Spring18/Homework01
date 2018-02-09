5774a803ae7da43609d6c7ba790b41dd35be4f9c
Greg <schaperg@moravian.edu>
2018-02-09 13:02:04 -0500

    original commit

---
+++ Project01/Main.java
@@ -0,0 +1,33 @@
+package Project01;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import Project01.Nation;
+import Project01.Tribe;
+import Project01.PlayGame;
+
+
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+       int gameLifePoints = 2000;
+       ArrayList<Nation> nations = new ArrayList<>() ; // nations create tribes create people
+       PlayGame game = new PlayGame();
+	   nations.add(new Nation("Idiots", (gameLifePoints / 2)));
+	   nations.add(new Nation("Minions", (gameLifePoints) / 2));
+	   for(int i = 0; i < 40; i++)
+       {
+           System.out.println("Round number: " + i);
+           if (game.playOneRound(nations))
+           {
+               System.out.println("There is a winner!");
+               break; // game over
+           }
+       }
+
+	   System.out.println("The winning nation is " + game.getWinner() + ".");
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+You are to create and submit a UML diagram for Project01.
+
+Due Date: Wednesday, February 14, at the beginning of class.
+
+Submit a printout of your UML diagram. Use LucidChart to 
+create your diagram. Make sure to provide a title that includes
+the assignment identification and your name.
